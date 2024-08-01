d3caee865565ae6ba86f759ac32e1f9a6ab4bcbb
AmirparsaDD <Amirparsab9@gmail.com>
2024-08-01 14:50:43 +0330

    Cleaned Up Code And Function Names

--- main.py
+++ main.py
@@ -16,33 +16,34 @@ OUTPUT_FOLDER = "./out"
 # Directories
 
 def main():
-  clear() # Just To Make Sure There Isn't A ./main.py Hanging At The Top
+  clear_stdout() # Just To Make Sure There Isn't A ./main.py Hanging At The Top
 
   files = [f for f in listdir(INPUT_FOLDER) if isfile(join(INPUT_FOLDER, f))]
   last_category = "uncategorized"
 
-  for file in files:
-    file_path = f"{INPUT_FOLDER}/{file}"
+  for file_name in files:
+    file_path = f"{INPUT_FOLDER}/{file_name}"
 
-    print(FILENAME_HEADER(file))
-    name = input(NEW_NAME_INPUT).strip() or file
+    print(FILENAME_HEADER(file_name))
+    name = input(NEW_NAME_INPUT).strip() or file_name
 
     category = input(CATEGORY_INPUT).strip() or last_category
     category = "uncategorized" if category == "/" else category
     last_category = category
     category_path = f"{OUTPUT_FOLDER}/{category}"
 
+    # Generate The Folder For That Category
     Path(category_path).mkdir(parents=True, exist_ok=True)
 
     # Only Adds The Extension When There Is A Custom Name
-    file_extension = f"{DOT + file.split(DOT)[len(file.split(DOT))-1] if file != name else EMPTY_STRING}"
+    file_extension = f"{DOT + file_name.split(DOT)[len(file_name.split(DOT))-1] if file_name != name else EMPTY_STRING}"
     new_filename = f"{name}{file_extension}"
     replace_file(file_path, f"{category_path}/{new_filename}")
 
-    clear()
+    clear_stdout()
 
-def clear():
-  system('cls' if osname=='nt' else 'clear')
+def clear_stdout():
+  system('cls' if osname == 'nt' else 'clear')
 
 if __name__ == "__main__":
   main()
